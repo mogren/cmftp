386a418bc5017a06e1c2dc971cd828a4f3bc5c4f
Claes Mogren <claes.mogren@accedo.tv>
2015-02-19 21:00:24 +0100

    Try to run the command

--- commands.go
+++ commands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"strings"
 )
 
@@ -13,8 +14,21 @@ var CommandList = map[string]string{
 func (c Client) RunCommand(cmd string) string {
 	words := strings.Fields(cmd)
 	if val, ok := CommandList[words[0]]; ok {
+		fmt.Println("homedir: ", c.homeDir)
 		fmt.Println("got: ", words)
-		return "run " + val
+		// TODO; Check if len > 1
+		shcmd := val
+		if len(words) > 1 {
+			shcmd += strings.Join(words[1:], " ")
+		}
+		// Run command...
+		fmt.Print("Trying to run '" + shcmd + "'")
+		out, err := exec.Command(shcmd).Output()
+		if err != nil {
+			return "fail"
+		}
+		fmt.Println("out: " + string(out))
+		return "run " + val + " and got " + string(out)
 	}
 	return "No such command!"
 }
